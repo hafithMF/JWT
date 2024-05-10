8cdfb7fc744fc4f11047b5052c77fed37ff2e7ce
mfarhannr <muhammad755262@smk.belajar.id>
2024-05-10 15:10:24 +0700

    express_jwt_auth

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const authRoute = require('./Routes/AuthRoute');
+app.use('/api/auth/', authRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  // Menjalankan PORT
+  console.log(`Server is running on port ${PORT}`);
+});
+
